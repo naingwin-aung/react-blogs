6caaa251f098d7b782c61ff557e8dc06ec127045
NaingWinAung <naingwinaung1710@gmail.com>
2025-11-22 22:03:13 +0700

    container changes

--- src/utils/Container.jsx
+++ src/utils/Container.jsx
@@ -1,6 +1,6 @@
 const Container = ({children}) => {
   return (
-    <div className="px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64">
+    <div className="px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-78">
       {children}
     </div>
   )
